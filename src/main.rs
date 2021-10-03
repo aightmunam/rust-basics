e70f7d7b67664da4f5ccca2144939ac0d2ff5974
Munam Mubashir <munam.malik58@gmail.com>
2021-10-03 14:45:44 +0500

    Rust Strings
    - String vs Slices
    - Converting between types
    - Creating slices and substrings
    - Combining strings
    - Understanding memory handling for strings
    
    Signed-off-by: Munam Mubashir <munam.malik58@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,88 @@
-use std::cmp::Ordering;
-use std::num::ParseIntError;
-use rand::Rng;
-
-const TOTAL_GUESSES_ALLOWED:usize = 10;
-
+#[allow(unused_variables)]
 fn main() {
-    play_guessing_game();
-}
+    // TWO DIFFERENT TYPES TO REPRESENT STRINGS - `&str`, `String`
+    // ------
 
-fn play_guessing_game() -> () {
-    let mut guesses_made:[i32; TOTAL_GUESSES_ALLOWED] = [-1; TOTAL_GUESSES_ALLOWED];
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    // Type 1: &str is a reference to an immutable storage of string. This can be a reference to some
+    // memory location in Heap, Stack, or directly in the binary.
 
-    let mut total_guesses_made = 0;
-    while total_guesses_made < TOTAL_GUESSES_ALLOWED {
-        let guess:i32 = match get_guess_from_user() {
-            Ok(num) => {
-                guesses_made[total_guesses_made] = num;
-                total_guesses_made += 1;
-                num
-            },
-            Err(_) => {
-                println!("Please enter a number!");
-                continue
-            },
-        };
-        check_if_user_won(guess, secret_number);
-    }
+    // String literals are stored in the binary, a reference to that memory location is saved as
+    // &str in the variable.
+    let s1: &str = "foo";
 
-    println!("Secret number: {}", secret_number);
-    display_user_guesses(guesses_made);
-}
+    // Fixed sized array of utf8 are stored in the stack. In this case, a reference to the stack
+    // location is saved as the &str in the variable.
+    let s1: &str= std::str::from_utf8(&[b'f', b'o', b'o']).expect("Error!");
 
-fn get_guess_from_user() -> Result<i32, ParseIntError> {
-    println!("Please enter a guess:");
+    // String data is stored on the heap. Rust automatically coerces type &String to &str
+    // where needed. Here, s3 stores a reference to the heap location where "foo" is stored.
+    let s1: &str = &String::from("foo");
 
-    let mut guess= String::new();
-    std::io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line.");
-    guess.trim().parse()
-}
+    // Type 2: String is a mutable collection that is stored on the heap.
+    let string1: String = String::from("foo");
 
-fn check_if_user_won(guess: i32, goal: i32) -> bool {
-    println!("\nYou guessed: {}", guess);
 
-    match guess.cmp(&goal) {
-        Ordering::Less => {
-            println!("You should guess higher!\n");
-            false
-        },
-        Ordering::Greater => {
-            println!("You should guess lower!\n");
-            false
-        },
-        Ordering::Equal => {
-            println!("Great Work! You guessed correctly!");
-            true
-        }
-    }
-}
+    // CONVERTING STRING TYPES
+    // ------
 
-fn display_user_guesses(guesses_made: [i32; TOTAL_GUESSES_ALLOWED]) {
-    println!("{} Guesses made in total\n", guesses_made.len());
-    let mut guess_number = 1;
-    for guess in guesses_made.iter() {
-        if *guess == -1 {
-            break;
-        }
-        println!("Guess #{}: {}", guess_number, guess);
-        guess_number += 1;
-    }
+    // Converting string slice to String
+    let string2: String = "foo".to_string();
+    let string2: String = s1.to_string();
+    let string2: String = String::from("foo");
+    let string2: String = String::from(s1);
+
+
+    // Converting String to a slice
+    // Rust automatically coerces the &String type to &str
+    let s2: &str = &string2;
+    let s2: &str = &string2[..];
+
+
+    // SUBSTRINGS
+    // ------
+
+    // Get a substring of a String
+    // &String[start..end] means get a slice starting at `start` upto but not including `end`
+    let s3: &str = &string2[0..]; // Get a slice from 0 to last index
+    let s3: &str = &string2[..2]; // Get a slice from start to 1 index (right index is excluded)
+    let s3: &str = &string2[0..3]; // Get a slice from 0 to 2 index
+
+
+    // COMBINING STRINGS
+    // ------
+
+    // Combining multiple string slices
+    let string3 = ["foo", "bar"].concat(); // Results in a String not in a string slice i.e `&str`
+    let string3 = format!("{}{}", "foo", "bar"); // Results in a String not a string slice
+
+    // If we want to combine a String and a string slice, we can add them simply
+    // However, this will only work if the String type is added first and then the slice(s)
+    let string3 = string3 + s1 + s2 + s3 + &string2;
+
+    // Combining multiple String types
+    // The first value should be a String and the rest should be String references i.e &String
+    let string3 = String::from("foo") + &String::from("bar") + &String::from("foobar");
+
+    // Adding to a String
+    let mut string4 = String::new();
+    string4.push_str(s3);
+    string4.push_str("bar");
+    string4.push_str(&string3);
+
+    // We can also add chars to a String
+    string4.push('f');
+    string4.push('o');
+    string4.push('o');
+
+
+    // GETTING CHARS FROM STRING
+    // ------
+
+    // We cannot get a char like &string[3] as a char could be more than a single letter.
+    // Gets the third character. This is not equivalent to string4[3]
+    let c = string4.chars().nth(3); // Returns an Option
+    let c = match c {
+        Some(c) => c,
+        None => {}
+    };
 }
